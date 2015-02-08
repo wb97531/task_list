2713009e3236e28b2f5ee01be6992438726999cc
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-02-08 02:29:42 -0700

    uncommented pg

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :doers
 
   resources :tasks
-	
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
