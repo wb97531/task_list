b15535f1cb7cb4ac3cc74895b6d2775a3ea6384e
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-02-08 23:12:41 -0700

    minor change

--- app/models/doer.rb
+++ app/models/doer.rb
@@ -4,5 +4,5 @@ class Doer < ActiveRecord::Base
 
 	validates_confirmation_of :password, if: lambda { |m| !m.omniauth? && m.password.present? }
 	validates_presence_of :password, :on => :create, if: lambda { |m| !m.omniauth? }
-	validates_presence_of :password_confirmation, if: lambda { |m |!m.omniauth? && m.password.present? }
+	validates_presence_of :password_confirmation, if: lambda { |m| !m.omniauth? && m.password.present? }
 end
